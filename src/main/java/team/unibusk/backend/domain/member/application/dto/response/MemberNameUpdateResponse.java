36f9a39dedfcf7247e97957370555aeb30637159
y22jun <tlsdpwns0612@naver.com>
2026-01-21 22:12:11 +0900

    Chore: MemberNameUpdateResponse 응답 값 변경

--- src/main/java/team/unibusk/backend/domain/member/application/dto/response/MemberNameUpdateResponse.java
+++ src/main/java/team/unibusk/backend/domain/member/application/dto/response/MemberNameUpdateResponse.java
@@ -7,7 +7,13 @@ import lombok.Builder;
 public record MemberNameUpdateResponse(
 
         @Schema(description = "이름이 수정된 회원의 ID", example = "1")
-        Long memberId
+        Long memberId,
+
+        @Schema(description = "이름이 수정된 회원 이메일", example = "hong@unibusk.com")
+        String email,
+
+        @Schema(description = "이름이 수정된 회원 이름", example = "홍길동동")
+        String name
 
 ) {
 }
