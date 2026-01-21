780b541e847c43fecbbd8781cb03fe8d90e21f8b
신예준 <151177624+y22jun@users.noreply.github.com>
2026-01-21 22:26:11 +0900

    Chore: MemberNameUpdateResponse 응답 값 변경 (#50)

--- src/main/java/team/unibusk/backend/domain/member/presentation/request/MemberNameUpdateRequest.java
+++ src/main/java/team/unibusk/backend/domain/member/presentation/request/MemberNameUpdateRequest.java
@@ -9,7 +9,7 @@ import team.unibusk.backend.domain.member.application.dto.request.MemberNameUpda
 @Builder
 public record MemberNameUpdateRequest(
 
-        @Schema(description = "새로운 회원 이름", example = "홍길동", maxLength = 15)
+        @Schema(description = "새로운 회원 이름", example = "홍길동동", maxLength = 15)
         @NotBlank(message = "이름을 입력해 주세요.")
         @Size(max = 15, message = "이름은 최대 15글자까지 작성 가능합니다.")
         String name
