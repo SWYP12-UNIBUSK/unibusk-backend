e14098677ec3fa84b46b6a952ba6930e1ddebe4c
y22jun <tlsdpwns0612@naver.com>
2026-03-15 23:26:01 +0900

    Refactor: LoggingAspect의 AfterThrowing 제거

--- src/main/java/team/unibusk/backend/global/logging/aspect/LoggingAspect.java
+++ src/main/java/team/unibusk/backend/global/logging/aspect/LoggingAspect.java
@@ -2,15 +2,12 @@ package team.unibusk.backend.global.logging.aspect;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
-import team.unibusk.backend.global.logging.exception.ExceptionLogger;
 import team.unibusk.backend.global.logging.masker.ArgumentMasker;
 
 import java.lang.reflect.Method;
@@ -22,7 +19,6 @@ import java.lang.reflect.Method;
 public class LoggingAspect {
 
     private final ArgumentMasker argumentMasker;
-    private final ExceptionLogger exceptionLogger;
 
     @Pointcut(
             "execution(* team.unibusk.backend.domain..presentation..*(..))"
@@ -32,23 +28,19 @@ public class LoggingAspect {
     @Around("cut()")
     public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
-
         Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
         String maskedArgs = argumentMasker.mask(joinPoint.getArgs());
 
         log.info("[CALL] {} args={}", method.getName(), maskedArgs);
 
-        Object result = joinPoint.proceed();
-
-        log.info("[END] {} took={}ms", method.getName(), System.currentTimeMillis() - start);
-
-        return result;
-    }
-
-    @AfterThrowing(pointcut = "cut()", throwing = "e")
-    public void afterThrowing(JoinPoint joinPoint, Throwable e) {
-        Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
-        exceptionLogger.log(method, e);
+        try {
+            Object result = joinPoint.proceed();
+            log.info("[END] {} took={}ms", method.getName(), System.currentTimeMillis() - start);
+            return result;
+        } catch (Throwable e) {
+            log.info("[ERR] {} took={}ms", method.getName(), System.currentTimeMillis() - start);
+            throw e;
+        }
     }
 
 }
