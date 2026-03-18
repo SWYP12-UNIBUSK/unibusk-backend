0cd36d43677c0460a2fdd00cc89ca46bc1919ea1
y22jun <tlsdpwns0612@naver.com>
2026-03-19 01:23:19 +0900

    Test: 컨트롤러 테스트 기반 클래스 설정 추가

---
+++ src/test/java/team/unibusk/backend/global/support/ControllerTestSupport.java
@@ -0,0 +1,26 @@
+package team.unibusk.backend.global.support;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+import team.unibusk.backend.global.config.TestSecurityConfig;
+import tools.jackson.databind.ObjectMapper;
+
+@ActiveProfiles("test")
+@Import({TestSecurityConfig.class, MethodValidationPostProcessor.class})
+@WebMvcTest(controllers = {
+
+})
+public abstract class ControllerTestSupport {
+
+    @Autowired
+    protected MockMvcTester mvcTester;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+}
+
