58224433908a7df56aa1a0b2b90d4e123099e86d
y22jun <tlsdpwns0612@naver.com>
2026-03-21 00:25:17 +0900

    Refactor: ControllerTestSupport에서 @WebMvcTest 제거

--- src/test/java/team/unibusk/backend/global/support/ControllerTestSupport.java
+++ src/test/java/team/unibusk/backend/global/support/ControllerTestSupport.java
@@ -1,7 +1,6 @@
 package team.unibusk.backend.global.support;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
@@ -11,9 +10,6 @@ import tools.jackson.databind.ObjectMapper;
 
 @ActiveProfiles("test")
 @Import({TestSecurityConfig.class, MethodValidationPostProcessor.class})
-@WebMvcTest(controllers = {
-
-})
 public abstract class ControllerTestSupport {
 
     @Autowired
