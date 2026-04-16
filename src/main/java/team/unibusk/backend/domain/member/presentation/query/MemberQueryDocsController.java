4065d38e456b2174aae09fb5e8cc472b1f968643
y22jun <tlsdpwns0612@naver.com>
2026-04-16 19:22:32 +0900

    Refactor: MemberQueryController 패키지를 presentation.query로 이동

--- src/main/java/team/unibusk/backend/domain/member/presentation/query/MemberQueryDocsController.java
+++ src/main/java/team/unibusk/backend/domain/member/presentation/query/MemberQueryDocsController.java
@@ -1,4 +1,4 @@
-package team.unibusk.backend.domain.member.presentation;
+package team.unibusk.backend.domain.member.presentation.query;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
